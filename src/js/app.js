b767f37fba9cb44b12d42e8bf469b820975db7ba
Svetlana Arkhipova <sve.tochka@inbox.ru>
2021-03-17 12:46:10 +0300

    eslint

--- src/js/app.js
+++ src/js/app.js
@@ -2,28 +2,28 @@ import setGoblin from './setgoblin';
 
 let win = 0;
 let fail = 0;
-let winEl = document.querySelector('span.win');
-let failEl = document.querySelector('span.fail');
+const winEl = document.querySelector('span.win');
+const failEl = document.querySelector('span.fail');
 
 setGoblin();
-let timer = setInterval(() => { 
-    document.querySelector('.goblin').classList.remove('goblin');
-    setGoblin();
+const timer = setInterval(() => {
+  document.querySelector('.goblin').classList.remove('goblin');
+  setGoblin();
 }, 1000);
 
 const field = document.querySelector('.field');
 field.addEventListener('click', (event) => {
-  let cell = event.target.closest('.goblin');
-    if (fail === 5) {
-      clearInterval(timer);
-      alert ('Игра закончена. Вы проиграли');
-      return;
-    }
-    if (!cell) {
-      fail += 1;
-      failEl.textContent = fail;
-      return;
-    }
-    win += 1;
-    winEl.textContent = win;
+  const cell = event.target.closest('.goblin');
+  if (fail === 5) {
+    clearInterval(timer);
+    alert('Игра закончена. Вы проиграли');
+    return;
+  }
+  if (!cell) {
+    fail += 1;
+    failEl.textContent = fail;
+    return;
+  }
+  win += 1;
+  winEl.textContent = win;
 });
