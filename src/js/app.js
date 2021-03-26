c1801f913258f413f9b11c0583367567d6e8fc9f
Svetlana Arkhipova <sve.tochka@inbox.ru>
2021-03-26 14:52:02 +0300

    handler added

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,5 @@
+import handlerFactory from './handler';
 import setGoblin from './setgoblin';
-
-let win = 0;
-let fail = 0;
-const winEl = document.querySelector('span.win');
-const failEl = document.querySelector('span.fail');
 
 setGoblin();
 const timer = setInterval(() => {
@@ -12,18 +8,4 @@ const timer = setInterval(() => {
 }, 1000);
 
 const field = document.querySelector('.field');
-field.addEventListener('click', (event) => {
-  const cell = event.target.closest('.goblin');
-  if (fail === 5) {
-    clearInterval(timer);
-    alert('Игра закончена. Вы проиграли');
-    return;
-  }
-  if (!cell) {
-    fail += 1;
-    failEl.textContent = fail;
-    return;
-  }
-  win += 1;
-  winEl.textContent = win;
-});
+field.addEventListener('click', handlerFactory(timer));
