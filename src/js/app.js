c6b6980ce15d05d5f414dc84deeccb7431a15277
Svetlana Arkhipova <sve.tochka@inbox.ru>
2021-04-13 17:08:29 +0300

    duration counter

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,24 @@
 import handlerFactory from './handler';
 import setGoblin from './setgoblin';
 
+let fail = 0;
+const failEl = document.querySelector('span.fail');
+
 setGoblin();
 const timer = setInterval(() => {
   document.querySelector('.goblin').classList.remove('goblin');
   setGoblin();
 }, 1000);
 
+const timer2 = setInterval(() => {
+  fail += 1;
+  failEl.textContent = fail;
+  if (fail === 5) {
+    clearInterval(timer);
+    clearInterval(timer2);
+    alert('Игра закончена. Вы проиграли');
+  }
+}, 1000);
+
 const field = document.querySelector('.field');
-field.addEventListener('click', handlerFactory(timer));
+field.addEventListener('click', handlerFactory(timer, timer2, fail, failEl));
--- src/js/handler.js
+++ src/js/handler.js
@@ -1,11 +1,10 @@
 let win = 0;
-let fail = 0;
 const winEl = document.querySelector('span.win');
-const failEl = document.querySelector('span.fail');
 
-export default function handlerFactory(timer) {
+export default function handlerFactory(timer, timer2, fail, failEl) {
   return (event) => {
     const cell = event.target.closest('.goblin');
+    clearInterval(timer2); // если кликнули, то остановить отсчет времени
     if (fail === 5) {
       clearInterval(timer);
       alert('Игра закончена. Вы проиграли');
@@ -18,5 +17,14 @@ export default function handlerFactory(timer) {
     }
     win += 1;
     winEl.textContent = win;
+    timer2 = setInterval(() => { // запустить отсчет времени
+      fail += 1;
+      failEl.textContent = fail;
+      if (fail === 5) {
+        clearInterval(timer);
+        clearInterval(timer2);
+        alert('Игра закончена. Вы проиграли');
+      }
+    }, 1000);
   };
 }
